f41c766a014663334b8ffbd4dd41a31dc78abb4e
604234050CS <604234050@parichat.skru.ac.th>
2019-01-18 18:57:53 +0700

    firstning

---
+++ src/providers/book-rest/book-rest.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Book } from '../../models/book.model';
+
+
+@Injectable()
+export class BookRestProvider {
+
+  private url="https://raw.githubusercontent.com/kesinee-bo/Images/master/cs_books.json";
+
+  constructor(public http: HttpClient) {
+    console.log('Hello BookRestProvider Provider');
+  }
+
+  getBookList() :Observable<any>
+  {
+    return this.http.get<Book>(this.url);
+  }
+
+}
